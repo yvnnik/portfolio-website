0809286c46be9a624f4cce3dfcf8cb402011f394
yvnnik <83207610+yvnnik@users.noreply.github.com>
2023-10-22 12:58:20 +0200

    added dark mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,12 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
+
+// Select the button
+const btn = document.querySelector('.btn-toggle');
+
+// Listen for a click on the button
+btn.addEventListener('click', function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle('dark-theme');  
+})
